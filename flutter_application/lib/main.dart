76c9c00130144d0b2aa68353f7bb56de992b0d74
Catherine <catherine.matta22399@gmail.com>
2021-01-24 02:19:41 -0500

    More styling

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,48 +9,44 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Carbomeasure',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lightGreen,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'CarboMeasure'),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Hello World',
+      theme: new ThemeData(),
+      home: HomePage(title: 'MeO2'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         backgroundColor: Colors.lightBlue[50],
+//         body: Center(
+//           child: Text(
+//             "MeO2",
+//             style: TextStyle(
+//                 fontSize: 50,
+//                 fontFamily: 'Sansation',
+//                 color: Colors.green[900]),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => new _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -71,47 +68,88 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
+    return new Scaffold(
+      appBar: new AppBar(
+        //backgroundColor: Color.fromRGBO(242, 252, 252, 1),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Image.asset('assets/images/MeO2.png'),
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        // title: new Text("MeO2",
+        //     style: TextStyle(
+        //         fontSize: 40,
+        //         fontFamily: 'Roboto',
+        //         color: Color.fromRGBO(70, 101, 81, 1))),
       ),
-      body: Center(
+      backgroundColor: Color.fromRGBO(242, 252, 252, 1),
+
+      body: new Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // Column is also layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
           //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
           //
           // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            new Text(
+              'Current Daily Total: ',
+              style: TextStyle(
+                  fontSize: 40, fontFamily: 'Roboto', color: Colors.green[900]),
             ),
-            Text(
+            new Text(
+              "\n\n\n\t\t\t\t\t\t\t\t\t\t\t\tkg",
+              style: TextStyle(
+                  fontFamily: 'Roboto', fontSize: 20, color: Colors.green[900]),
+            ),
+            new Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              //style: Theme.of(context).textTheme.headline1,
+              style: TextStyle(
+                  fontSize: 100,
+                  fontFamily: 'Roboto',
+                  color: Colors.green[900]),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      //       RaisedButton(
+      //   shape: StadiumBorder(),
+      //   onPressed: _incrementCounter,
+      //   child: Text("Button"),
+      // )
+      floatingActionButton: FloatingActionButton.extended(
+        // FloatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        backgroundColor: Color.fromRGBO(54, 140, 114, .10),
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        label: Text(
+          "Add CO2",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Color.fromRGBO(70, 101, 81, 1)),
+        ),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget build(BuildContext context) {
+  return Image(image: AssetImage('assets/images/download.png'));
 }
